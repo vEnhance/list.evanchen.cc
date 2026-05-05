856aec3cd79ce0cdfb10ecc7723845032e56de8c
Evan Chen <evan@evanchen.cc>
2026-05-05 10:33:41 -0400

    polish: add help text

--- mailing/models.py
+++ mailing/models.py
@@ -12,14 +12,29 @@ def generate_token():
 
 class SubscriberEmail(models.Model):
     email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    subscribed_blog = models.BooleanField(default=False, verbose_name="Blog")
-    subscribed_wall = models.BooleanField(default=False, verbose_name="Wall")
-    is_new = models.BooleanField(default=True)
-    google_authenticated = models.BooleanField(default=False, verbose_name="Google")
+    subscribed_blog = models.BooleanField(
+        default=False,
+        verbose_name="Blog",
+        help_text="Whether the user will get emails for blog.evanchen.cc",
+    )
+    subscribed_wall = models.BooleanField(
+        default=False,
+        verbose_name="Wall",
+        help_text="Whether the user will get emails for blog.evanchen.cc",
+    )
+    is_new = models.BooleanField(
+        default=True,
+        help_text="Changes to False once the user saves their subscription for the first time.",
+    )
+    google_authenticated = models.BooleanField(
+        default=False,
+        verbose_name="Google",
+        help_text="Whether Google has authenticated this email address",
+    )
     token = models.CharField(max_length=24, unique=True, default=generate_token)
-    name = models.CharField(max_length=255, blank=True)
     custom_greeting = models.TextField(
         blank=True,
         help_text="A customized greeting for the megaphone. "
