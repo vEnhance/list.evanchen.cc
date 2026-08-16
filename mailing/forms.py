02f16364784dc3562e9d5a469e5d65066784f18e
Evan Chen <evan@evanchen.cc>
2026-08-15 23:10:59 -0700

    chore: that's ruff buddy
    joke stolen from cj in
    https://github.com/vEnhance/otis-web/pull/524#issuecomment-5274464573

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,3 +1,5 @@
+from typing import ClassVar
+
 from django import forms
 from django.utils.safestring import mark_safe
 
@@ -7,8 +9,8 @@ from .models import SubscriberEmail
 class EditSubscriptionForm(forms.ModelForm):
     class Meta:
         model = SubscriberEmail
-        fields = ["subscribed_blog", "subscribed_wall"]
-        labels = {
+        fields = ("subscribed_blog", "subscribed_wall")
+        labels: ClassVar[dict[str, str]] = {
             "subscribed_blog": mark_safe(
                 '<a href="https://blog.evanchen.cc">blog.evanchen.cc</a>'
             ),
@@ -16,7 +18,7 @@ class EditSubscriptionForm(forms.ModelForm):
                 '<a href="https://wall.evanchen.cc">wall.evanchen.cc</a>'
             ),
         }
-        help_texts = {
+        help_texts: ClassVar[dict[str, str]] = {
             "subscribed_blog": "Long posts every 1-3 months. Probably what you want.",
             "subscribed_wall": "Daily shower thoughts. Super noisy. Use with care.",
         }
